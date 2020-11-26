256ccf8caee0625ef66527f92a82987842d41e5f
Himanshu Pant <himanshupant127@gmail.com>
2020-11-25 21:31:27 -0500

    feat: Created an event driver version

--- main.js
+++ main.js
@@ -1,7 +1,81 @@
+const events = require('events');
+const util = require('util');
 const md5 = require('./md5');
 const rq = require('./request');
 
-module.exports = function(username = '') {
+const TempMailBoxEventDriven = function(username = '', options = {}) {
+  // Destructure interval from options.
+  const { interval = 1000 } = options;
+  // this class variable will track the ready state
+  this.ready = false;
+  // this class variable will track all the emails that are recieved
+  this.emails = [];
+  let error = false;
+  this.email = null;
+
+  // Suscribe to the ready event within itself
+  this.on('ready', () => {
+    this.ready = true;
+    // Create a setInterval to keep tracking changes
+    setInterval(() => {
+      rq(`/request/mail/id/${this.emailHash}/format/json`, (rs, error) => {
+        // Check if the emails on the server are the same as the ones tracked
+        if(rs.length != this.emails.length && !rs.error) {
+          const newEmails = [];
+          // Filter out emails that are not in the tracked emails
+          rs.forEach((email) => {
+            let alreadyExists = false;
+            // Loop through the saved emails and check if the current email already exists
+            for(let i = 0; i < this.emails.length; i++) {
+              if(this.emails[i]._id.$oid === email._id.$oid) {
+                alreadyExists = true;
+                break;
+              }
+            }
+            if(!alreadyExists) {
+              // push the new email to saved Emails
+              newEmails.push(email);
+            }
+          });
+          if(newEmails.length > 0) {
+            this.emit("new", newEmails);
+          }
+          // Set the emails to that of response
+          this.emails = rs;
+        }
+        if(error) {
+          // Emit the error event
+          this.emit('error', error);
+        }
+      });
+    }, interval);
+  });
+
+  // Fire the setup request
+  rq(`/request/domains/format/json`, (domains, err) => {
+    if (!err) {
+      // Check if the username provided contains the @ symbol
+      if (username.includes('@')){
+        // Only take the first part of the username with @ symbol
+        username = username.split('@')[0];
+      }
+      // Set the values of the email and email Hash
+      this.email = (username.replace(/\ /g, '.') + domains[Math.floor(Math.random() * (domains.length - 1))]).toLowerCase();
+      this.emailHash = md5(this.email);
+      // Emit the ready event.
+      this.emit('ready');
+    } else {
+      error = err;
+      // Emit the error event.
+      this.emit('error', error);
+    }
+  });
+}
+
+// Make the TempMailBox constructor inherit event emitter
+util.inherits(TempMailBoxEventDriven, events.EventEmitter);
+
+const TempMailBox = function(username = '') {
   let error = false;
   let email = false;
   let emailHash = '';
@@ -50,4 +124,10 @@ module.exports = function(username = '') {
       readyHandlers.forEach((c) => c(null, error));
     }
   });
+}
+
+// Export the TempMailBox and TempMail class
+module.exports = {
+  TempMailBox,
+  TempMailBoxEventDriven
 }
--- test.js
+++ test.js
@@ -1,6 +1,32 @@
-const TM = require('./main');
+const { TempMailBox, TempMailBoxEventDriven } = require('./main');
+const CHANCE = require('chance');
 
-const email1 = new TM('temp-email'); // It will create an address like "temp-email@xxxxxx.xxx"
+const chance = new CHANCE();
+/**
+ * This is the event driven mailbox section
+ */
+// This will generate a random first name and then a random email.
+const eventDrivenInbox = new TempMailBoxEventDriven(chance.first());
+
+// Waiting for the email to be ready (recommended)
+eventDrivenInbox.on('ready', () => {
+  console.log(`Your event driven email inbox is ready!`);
+  console.log(eventDrivenInbox);
+});
+
+eventDrivenInbox.on('new', (newEmails) => {
+  console.log("Received new Emails in event driven inbox");
+  console.table(newEmails);
+});
+
+eventDrivenInbox.on('error', (error) => {
+  console.error(`Error in event driven inbox ${error}`);
+});
+
+/**
+ * This is the regular temp-mail box section
+ */
+const email1 = new TempMailBox('temp-email'); // It will create an address like "temp-email@xxxxxx.xxx"
 
 // Waiting for the email to be ready (recommended)
 email1.ready((email, error) => {
